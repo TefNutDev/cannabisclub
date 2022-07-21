ef76a3e00fb30949c88fa59d9c8ebbf46fd1d9e3
TefNutDev <evanmeleady@icloud.com>
2022-07-21 16:13:57 +0200

    commit on main

--- components/SliderData.js
+++ components/SliderData.js
@@ -2,30 +2,30 @@
 export const SliderData = [
     {
         image:
-        'https://images.unsplash.com/photo-1589844428386-06bba62381a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL19.jpg?raw=true',
     },
     {
         image:
-        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL13.jpg',
-    },
-
-    {
-        image:
-        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL14.jpg',
+        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL13.jpg?raw=true',
     },
 
     {
         image:
-        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL15.jpg',
+        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL14.jpg?raw=true',
     },
 
     {
         image:
-        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL17.jpg',
+        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL15.jpg?raw=true',
+    },
+
+    {
+        image:
+        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL17.jpg?raw=true',
     },
     {
         image:
-        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/membercard.jpg',
+        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/membercard.jpg?raw=true',
     },
 
 
