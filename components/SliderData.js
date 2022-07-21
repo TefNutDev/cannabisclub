163d64dadce8d9140a89d05effa93570b2a4c8b8
TefNutDev <evanmeleady@icloud.com>
2022-07-21 15:10:28 +0200

    commit fix images

--- components/SliderData.js
+++ components/SliderData.js
@@ -2,26 +2,26 @@
 export const SliderData = [
     {
         image:
-        '/../public/assets/thcL19.jpg'
+        '/public/assets/thcL19.jpg'
     },
     {
         image:
-        '/../public/assets/thcL13.jpg'
-    },
-
-    {
-        image:
-        '/../public/assets/thcL14.jpg'
+        '/public/assets/thcL13.jpg'
     },
 
     {
         image:
-        '/../public/assets/thcL15.jpg'
+        '/public/assets/thcL14.jpg'
     },
 
     {
         image:
-        '/../public/assets/thcL17.jpg'
+        '/public/assets/thcL15.jpg'
+    },
+
+    {
+        image:
+        '/public/assets/thcL17.jpg'
     },
     {
         image:
