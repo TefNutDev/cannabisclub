e3fa72b3988316971f3e440d07bdc2229a058e21
TefNutDev <evanmeleady@icloud.com>
2022-07-21 16:11:22 +0200

    commit on main

--- components/SliderData.js
+++ components/SliderData.js
@@ -2,7 +2,7 @@
 export const SliderData = [
     {
         image:
-        'https://github.com/TefNutDev/cannabisclub/blob/main/public/assets/thcL19.jpg',
+        'https://images.unsplash.com/photo-1589844428386-06bba62381a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     },
     {
         image:
